571517524015f7d092587f7e174cd8f428845e46
DigitalSecurityMod <12garmi@gmail.com>
2025-03-09 23:46:16 -0500

    fixed api_interaction. printing raw JSON data

--- api_interaction.py
+++ api_interaction.py
@@ -3,44 +3,52 @@ import json
 from dotenv import load_dotenv
 import os
 
-#Load environment variables from .env
-load_dotenv()
-
-#Get API key
-API_KEY = os.getenv('GOOGLE_CIVIC_API_KEY')
-ZIP_CODE = '60622' #Example zip code
-
+#Construct URL for Google Civic API
 def constructURL ():
+    #Load environment variables from .env
+    load_dotenv()
+    API_KEY = os.getenv('GOOGLE_CIVIC_API_KEY')
     base_url = "https://www.googleapis.com/civicinfo/v2/representatives"
     url = f"{base_url}?key={API_KEY}"
-    if ZIP_CODE:
-        url += f"&address={ZIP_CODE}"
+    ZIP_CODE = '60622' #Example zip code
+    url += f"&address={ZIP_CODE}"
     return url
 
+#Send request to Google Civic API
 def sendRequest (url):
     response = requests.get(url)
     return response
 
+#Check response from Google Civic API
 def checkResponse (response):
     try:
         response.raise_for_status()
         return True
-    except requests.exceptions.HTTPError:
+    except requests.exceptions.HTTPError as e:
+        if 'http' in str(e):
+            e = str(e).split('?')
+        else: 
+            return None
+        print(f"Error: Unable to fetch data from API {e[0]}")
         return False
 
-def parseResponse (response):
-    data = response.json()
+#Gets the representative data for the given zip code
+def getRepresentatives ():
+    url = constructURL()
+    response = sendRequest(url)
+    checkResponse(response)
+    return response.json()
 
-def printResponse (data):
+#Display the representative data
+def displayRepresentatives (data):
     print(json.dumps(data, indent=4))
 
 def main ():
-    url = constructURL()
-    response = sendRequest(url)
-    if checkResponse(response):
-        data = parseResponse(response)
-        printResponse(data)
+    data = getRepresentatives()
+    if data:
+        displayRepresentatives(data)
     else:
-        print("Error: Unable to fetch data from API")
+        return None
+
 if __name__ == "__main__":
     main()
